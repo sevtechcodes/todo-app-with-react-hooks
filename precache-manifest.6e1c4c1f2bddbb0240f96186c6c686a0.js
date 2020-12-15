9a3692eb7f8513d77071acc6e6ae1fd5d7490c10
Sevim <sevimtas0101@gmail.com>
2020-12-16 01:20:25 +0530

    Updates

---
+++ precache-manifest.6e1c4c1f2bddbb0240f96186c6c686a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44457063487f76d30cb1862388781c2d",
+    "url": "/sevtechcodes/todo-app-with-react-hooks.git/index.html"
+  },
+  {
+    "revision": "e2c992928aca6ef3e782",
+    "url": "/sevtechcodes/todo-app-with-react-hooks.git/static/css/main.b1e8f748.chunk.css"
+  },
+  {
+    "revision": "e7563aaee5e8f22c2506",
+    "url": "/sevtechcodes/todo-app-with-react-hooks.git/static/js/2.4b470c50.chunk.js"
+  },
+  {
+    "revision": "e2c992928aca6ef3e782",
+    "url": "/sevtechcodes/todo-app-with-react-hooks.git/static/js/main.0d5e3fe8.chunk.js"
+  },
+  {
+    "revision": "5988c2c291abac58dffa",
+    "url": "/sevtechcodes/todo-app-with-react-hooks.git/static/js/runtime-main.4e5b2c55.js"
+  }
+]);
